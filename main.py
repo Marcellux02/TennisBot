d474a4345e246ba485bab139b4eaa61726548f50
Marcello Mendo <38981901+Marcellux02@users.noreply.github.com>
2022-05-07 14:40:29 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 from telegram.ext import *
+import data
+import json
 import risposte as R
-import costanti
-API_KEY = costanti.API_KEY
-updater = Updater(API_KEY, use_context=True)
+
+updater = Updater(data.API_KEY, use_context=True)
+
+
+def updateVersion(nUpdate, description):
+    x = {
+        "versione": str(nUpdate),
+        "descrizione": str(description)
+    }
+    if nUpdate is None or description is None:
+        return
+    else:
+        try:
+            with open('versioni.json', 'r+') as f:
+                temp = json.load(f)
+                temp["versioni"].append(x)
+                f.seek(0)
+                json.dump(temp, f, indent=2)
+        except Exception as e:
+            print('Errore nell\'apertura del file ' + str(e))
 
 
 def main():
+    # UpdateVersion viene modificato internamente quando c'è un nuovo aggiornamento.
+    updateVersion(None, None)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("help_gang", R.help_message))
     dp.add_handler(CommandHandler("menuleoncino", R.menu_leoncino))
--- risposte.py
+++ risposte.py
@@ -1,19 +1,13 @@
+import json
 import random
 import telegram
-import costanti
+import data
 from telegram import InputMediaPhoto
 
 # update.message.reply_photo(photo=open("Foto18/.../" + str(prova[rand]), "rb"))
 # bot.send_media_group(media=InputMediaPhoto(open(path, 'rb'))
 #                      , chat_id=update.message.chat_id)
-
-aggiornamenti = ["Versione 1.0.0 \nVersione iniziale del bot, pieno di bug ma arriveranno aggiornamenti",
-                 "Versione 1.0.1 \nAggiunti vari comandi e risolto alcuni bug"]
-API_KEY = costanti.API_KEY
-bot = telegram.Bot(token=API_KEY)
-tette = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg",
-         "11.jpg", "12.jpg", "13.jpg", "14.jpg", "15.jpg"]
-culo = ["1.jpg", "2.jpg", "3.jpg", "4.jpg", "5.jpg", "6.jpg", "7.jpg", "8.jpg", "9.jpg", "10.jpg"]
+bot = telegram.Bot(token=data.API_KEY)
 
 
 def handle_message(update, context):
@@ -21,11 +15,11 @@ def handle_message(update, context):
     print(text)
     if "culo" in text:
         rand = random.randint(0, 9)
-        path = str("Foto18/Culo/" + str(culo[rand]))
+        path = str("Foto18/Culo/" + str(data.C[rand]))
         update.message.reply_photo(photo=open(path, "rb"))
     if "tette" in text:
         rand = random.randint(0, 14)
-        path = str("Foto18/Tette/" + str(tette[rand]))
+        path = str("Foto18/Tette/" + str(data.T[rand]))
         update.message.reply_photo(photo=open(path, "rb"))
 
 
@@ -63,8 +57,15 @@ def comandi(update, context):
 
 
 def versione_bot(update, context):
-    for aggiornamento in aggiornamenti:
-        update.message.reply_text(aggiornamento)
+    with open('versioni.json', 'r') as f:
+        cronologia_aggiornamenti = json.load(f)
+        final_message = ""
+    for agg in cronologia_aggiornamenti["versioni"]:
+        v = agg["versione"]
+        d = agg["descrizione"]
+        temp_message = str("Versione " + v + "\n" + d)
+        final_message += temp_message + "\n" + "\n"
+    update.message.reply_text(final_message)
 
 
 def documentazione(update, context):
